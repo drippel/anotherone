fba59a3bb9b4b7d8677e53005c6b35cc2a7e381d
Dan Rippel <drippel@crossview.com>
2017-12-27 13:36:27 -0500

    day 15 part 2 done

--- src/main/scala/com/github/aoc/Day15.scala
+++ src/main/scala/com/github/aoc/Day15.scala
@@ -5,14 +5,25 @@ object Day15 {
   def main( args : Array[String] ) : Unit = {
     Console.println( "day 15..." )
     
-    var startA = 65L
-    var startB = 821L 
-    // var startA = 679L
-    // var startB = 771L 
+    // var startA = 65L
+    // var startB = 8921L 
+    var startA = 679L
+    var startB = 771L 
+    
+
+    /*
+    for( i <- 0 until 5 ){
+      startB = genB( startB ) 
+      Console.println( "Found:"+ startB )
+      val a = sub( pad( startB ) )
+      Console.println( a )
+    }
+    * 
+    */
     
     var count = 0
     
-    for( i <- 0 until 40000000 ){
+    for( i <- 0 until 5000000 ){
       startA = genA( startA ) 
       startB = genB( startB ) 
       val a = sub( pad( startA ) )
@@ -25,6 +36,7 @@ object Day15 {
     
     Console.println( count )
     
+    
   }
   
   def sub( s : String ) : String = {
@@ -35,13 +47,25 @@ object Day15 {
     l.toBinaryString.reverse.padTo(64, '0').reverse
   }
   
-  def prodFunc( factor : Long)( a : Long ) : Long = {
+  def prodFunc( factor : Long)( mult : Long )( a : Long ) : Long = {
     
-    val c = a * factor
-    val r = c % 2147483647L
-    r
+    def inner( l : Long ) : Long = {
+    
+      val c = l * factor
+      val r = c % 2147483647L
+      
+      if( r % mult == 0 ){
+        r
+      }
+      else {
+        inner( r )
+      }
+
+    }
+    
+    inner( a )
   }
   
-  val genA = prodFunc( 16807 )( _ )
-  val genB = prodFunc( 48271 )( _ )
+  val genA = prodFunc( 16807 )( 4L )( _ )
+  val genB = prodFunc( 48271 )( 8L )( _ )
 }
