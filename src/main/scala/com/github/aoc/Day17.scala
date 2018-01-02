bde6e01cb797f61672ff2df59eb2629ffdd9b296
Dan Rippel <drippel@crossview.com>
2018-01-02 10:42:49 -0500

    day 17 part 2 done

--- src/main/scala/com/github/aoc/Day17.scala
+++ src/main/scala/com/github/aoc/Day17.scala
@@ -5,23 +5,40 @@ object Day17 {
   val limit = 50000000 
   
   var listSize = 0
-  var pos = 0
-  var last = -1
+  var listPos = 0
+  var listLast = -1
   
   def main( args : Array[String] ) : Unit = {
     Console.println( "day 17..." )
     
-    for( i <- 1 to 50000 ){
+    // var pos = 0
+    // val list = CList.createCList( List(0) )
+
+    listSize = 1
+    listPos = 0
     
-      pos = calcPos( 380 )
-      pos = pos + 1
-      insert( pos, i )
+    for( i <- 1 to limit ){
+    
+      // pos = CList.calcPos( list, pos, 380 )
+      listPos = calcPos( listPos, 380 )
+      // Console.println( "p:"+ pos +" lp:"+ listPos )
+
+      // pos = pos + 1
+      listPos = listPos + 1
+
+      // CList.insert( list, pos, i )
+      insert( listPos, i )
+
+      // if( pos == 1 ){
+        // Console.println( list )
+      // }
     }
 
-    Console.println( "Size:"+ listSize +" Pos:"+ pos  +" Last:"+ last )
+    // Console.println( "Pos:"+ pos  )
+    Console.println( "Size:"+ listSize +" Pos:"+ listPos  +" Last:"+ listLast )
   }
   
-  def calcPos[T]( offset : Int ) : Int = {
+  def calcPos[T]( pos : Int, offset : Int ) : Int = {
       
     var np = pos
     for( i <- offset until 0 by -1 ){
@@ -38,7 +55,8 @@ object Day17 {
   def insert(pos : Int, value : Int ) : Unit = {
       
     if( pos == 1 ){
-      last = value
+      listLast = value
+      Console.println( listLast )
     }
 
     listSize = listSize + 1
